7c00e138a8295e9eefe7c19b3620d579ee593111
LeoRiether <leonardoalves8217@gmail.com>
2022-09-14 18:28:16 -0300

    AES example for manual testing

--- scripts/aes_example.py
+++ scripts/aes_example.py
@@ -1,11 +1,9 @@
 import sign.aes as aes
 import secrets
 
-data = bytearray(b'Helloooo Woooooorld!!!!!!!!!')
-key = secrets.randbits(128).to_bytes(16, 'big')
-
-aes.encrypt_ctr(data, key, nonce=1)
-print(data)
-
-
+with open('requirements.txt', 'rb') as f:
+    key = secrets.randbits(128).to_bytes(16, 'big')
+    nonce = secrets.randbits(128)
+    g = aes.stream_ctr(f, key, nonce)
+    print(bytes.join(b'', g).hex())
 
