c8c9bc670a4d1b01ca7d4b121fda269b1850e2d4
LeoRiether <leonardoalves8217@gmail.com>
2022-09-14 18:40:15 -0300

    Update AES example

--- scripts/aes_example.py
+++ scripts/aes_example.py
@@ -2,7 +2,7 @@ import sign.aes as aes
 import secrets
 
 with open('requirements.txt', 'rb') as f:
-    key = secrets.randbits(128).to_bytes(16, 'big')
+    key = secrets.token_bytes(128 // 8)
     nonce = secrets.randbits(128)
     g = aes.stream_ctr(f, key, nonce)
     print(bytes.join(b'', g).hex())
